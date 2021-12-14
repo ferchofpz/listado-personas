545d587b3eb1518f0a8c6c9476752b01b2e5b15c
Fernando Perez <ferchofpz@hotmail.com>
2021-12-14 15:40:33 -0500

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Persona } from './persona.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Listado de personas';
+  personas:Persona[] = [new Persona('Juan','Perez'), new Persona('Laura','Juarez'),new Persona('Carla','Lara')];
+}
