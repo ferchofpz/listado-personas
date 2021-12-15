e8c32686730f739540c1322ab6adc0a9b703b725
Fernando Perez <ferchofpz@hotmail.com>
2021-12-15 09:50:15 -0500

    Local reference y ViewChild

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Persona } from '../persona.model';
 
 @Component({
@@ -11,11 +11,15 @@ export class FormularioComponent{
   // Event binding de hijo hacia padre
   @Output() personaCreada = new EventEmitter<Persona>();
 
-  nombreInput:string='';
-  apellidoInput:string='';
+  // nombreInput:string='';
+  // apellidoInput:string='';
+
+  // Se recupera la referencia definida en la plantilla
+  @ViewChild('nombreInput') nombreInput: ElementRef;
+  @ViewChild('apellidoInput') apellidoInput: ElementRef;
 
   agregarPersona():void{
-    let persona1 = new Persona(this.nombreInput,this.apellidoInput);
+    let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
     // this.personas.push(persona1);
     this.personaCreada.emit(persona1);
   }
