6824bb168bd3bd18e3e6f5355991b1062a4cfaca
Fernando Perez <ferchofpz@hotmail.com>
2021-12-18 08:58:17 -0500

    Router link y paso de parámetros

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -18,4 +18,17 @@ export class PersonasService{
         this.loggingService.enviarMensajeAConsola("Se envia persona al arreglo: " + persona.nombre + ' ' + persona.apellido);
         this.personas.push(persona);
       }
+
+    encontrarPersona(index:number){
+        let persona: Persona = this.personas[index];
+        return persona;
+    }
+
+    modificarPersona(index:number, persona:Persona){
+
+        // Se modifica el registro dado que se esta trabajando con referencia
+        let persona1 = this.personas[index];
+        persona1.nombre = persona.nombre;
+        persona1.apellido = persona.apellido;
+    }
 }
--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Persona } from '../../persona.model';
 import { PersonasService } from '../../personas.service';
 
@@ -8,25 +8,41 @@ import { PersonasService } from '../../personas.service';
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent{
+export class FormularioComponent implements OnInit{
 
   // Event binding de hijo hacia padre
   // @Output() personaCreada = new EventEmitter<Persona>();
 
   nombreInput:string='';
   apellidoInput:string='';
+  index:number;
 
   // Se recupera la referencia definida en la plantilla
   // @ViewChild('nombreInput') nombreInput: ElementRef;
   // @ViewChild('apellidoInput') apellidoInput: ElementRef;
 
   // DI (dependency injection): Angular automaticamente inyecta una instancia de la clase
-  constructor(private personasService: PersonasService, private router:Router){
+  constructor(
+    private personasService: PersonasService, 
+    private router:Router,
+    private route:ActivatedRoute
+    ){
     
     // Suscripción al evento
     this.personasService.saludar.subscribe(
       (indice:number) => alert('El indice es: ' + indice)
     );
+  }
+  ngOnInit(): void {
+    // Se recupera el indice de la URL (ruta)
+    this.index = this.route.snapshot.params['id'];
+
+    // Si el indice no es nulo estamos modificando
+    if(this.index){
+      let persona: Persona = this.personasService.encontrarPersona(this.index);
+      this.nombreInput = persona.nombre;
+      this.apellidoInput = persona.apellido;
+    }
   }
 
   onGuardarPersona():void{
@@ -38,7 +54,12 @@ export class FormularioComponent{
     // this.loggingService.enviarMensajeAConsola("Enviamos persona: " + persona1.nombre + ' ' + persona1.apellido);
     // this.personaCreada.emit(persona1);
 
-    this.personasService.agregarPersona(persona1);
+    if(this.index){
+      this.personasService.modificarPersona(this.index, persona1);
+    }
+    else{
+      this.personasService.agregarPersona(persona1);
+    }
     this.router.navigate(["personas"]);
   }
 
